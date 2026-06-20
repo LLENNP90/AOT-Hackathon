eaa045bd4b9b76f27266b583156433192d2a8213
weiming1055 <wayneming06@gmail.com>
2026-06-20 16:14:20 +0800

    i did user backend free

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.js"
 
 dotenv.config();
 
@@ -18,3 +19,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
+
+app.use("/api/user", userRouter)
