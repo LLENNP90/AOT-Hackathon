29b959664d0e35eef7ff6e05f51e705f3bc09619
Jun Han <berlinh0r1@gmail.com>
2026-06-20 11:40:39 +0800

    1

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -5,8 +5,9 @@ export default function login() {
   return (
     <div className="font-Arial bg-[#0a0d36] flex justify-center items-center h-screen m-0">
       <div className="bg-[#05081C] p-8 rounded shadow-md w-full max-w-sm">
-        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <h2 className="text-2xl font-bold flex justify-center items-center">Login</h2>
         <form>
+
           <div className="username mt-3">
             <label className="block text-gray-700 text-sm font-bold">
               Username
@@ -34,12 +35,11 @@ export default function login() {
           
 
           <div className="flex items-center justify-between mt-4">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-            >
+            
+              <Link href ="/main" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button">
               Sign In
-            </button>
+            </Link>
             <Link href="/signup" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
               Don't have an account? Sign Up
             </Link>
