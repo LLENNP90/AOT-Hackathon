ea873c8f5ea07cf41b5e0d7debfc8e9c7506018c
Jun Han <berlinh0r1@gmail.com>
2026-06-20 11:25:56 +0800

    temp

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -3,19 +3,49 @@ import Link from 'next/link';
 
 export default function login() {
   return (
-    <div>login
-        <Link 
-        href="/" 
-        style={{ 
-          padding: '8px 16px', 
-          backgroundColor: '#000', 
-          color: '#fff', 
-          borderRadius: '4px',
-          textDecoration: 'none'
-        }}
-      >
-        Login
-      </Link>
-    </div>
+    <div className="font-Arial bg-[#0a0d36] flex justify-center items-center h-screen m-0">
+      <div className="bg-[#05081C] p-8 rounded shadow-md w-full max-w-sm">
+        <h2 className="text-2xl font-bold mb-4">Login</h2>
+        <form>
+          <div className="username">
+            <label className="block text-gray-700 text-sm font-bold">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+
+          
+          <div className="password">
+            <label className="block text-gray-700 text-sm font-bold">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          
+
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <Link href="/register" className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
+              Don't have an account? Sign Up
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div> 
   )
 }
